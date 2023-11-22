195cd8ad36b928f83b43a7fe39124b7dc04cfff5
sairam042 <140315270+sairam042@users.noreply.github.com>
2023-11-22 16:06:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+let para = document.getElementById("status");
+let button = document.getElementById("enterBtn");
+
+button.addEventListener("click",function(){
+	const h1Tag = document.createElement("h1");
+	h1Tag.textContent = 'Entered Metaverse';
+	para.parentNode.replaceChild(h1Tag,para);
+});
